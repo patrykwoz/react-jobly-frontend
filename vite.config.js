da8efd476fa4456c8c0ba1587fc74a4f4723a84c
Patryk Wozniczka <patryk.wozniczka@gmail.com>
2024-06-12 22:59:16 -0400

    Configure for git deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-jobly-frontend/',
   plugins: [react()],
   test: {
     environment: 'jsdom',

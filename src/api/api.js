5b4e075f75dcc49c515d17d17a37b7759118b1a5
Patryk Wozniczka <patryk.wozniczka@gmail.com>
2024-06-12 22:44:33 -0400

    Added static json

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = import.meta.env.REACT_APP_BASE_URL || "https://patryk-jobly-backend-4938d35eb531.herokuapp.com";
 
 /** API Class.
  *
